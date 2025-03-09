1c8582e61bd8e3aa247b51f042d08e90d1d82128
Matthew van der Hoorn <md.registrations@protonmail.com>
2025-03-09 16:45:51 +0100

    Earlier versions

---
+++ control.lua
@@ -0,0 +1,123 @@
+local enable_tech_list = {
+  "fuel-processing",
+  "electricity",
+  "electric-mining",
+  "basic-fluid-handling",
+  "automation",
+  "bio-processing-brown",
+  "basic-chemistry",
+  "bio-paper-1",
+  "angels-solder-smelting-basic",
+  "tech-red-circuit",
+  "angels-components-mechanical-1",
+  "angels-basic-blocks-1",
+  "ore-crushing",
+  "bob-logistics",
+  "basic-logistics-2",
+  "logistics-0",
+  "logistics",
+  "long-inserters-1"
+}
+
+local function research_tech()
+  for _, force in pairs(game.forces) do
+    for _, tech_name in pairs(enable_tech_list) do
+      if force.technologies[tech_name] then
+        force.technologies[tech_name].researched = true
+      end
+    end
+  end
+end
+
+local function place_item_nearby_player(item_name)
+  local surface = game.surfaces["nauvis"]
+
+  local center_search = {x = 0, y = 0 - 32}
+  local radius = 8
+  local precision = 0.01
+  local force_to_tile_center = true
+
+  local place_position = surface.find_non_colliding_position(item_name, center_search, radius, precision, force_to_tile_center)
+  return surface.create_entity({name=item_name, position=place_position, force="player"})
+end
+
+local function spawn_new_chest() 
+  return place_item_nearby_player("iron-chest")
+end
+
+local function give_items_to_player_through_chests(items)
+  local chest = spawn_new_chest()
+  for _, item in pairs(items) do
+    local item_name, count = item[1], item[2]
+    local item_prototype = game.item_prototypes[item_name]
+    if item_prototype then
+      local stack_size = item_prototype.stack_size
+      while (count > 0) do
+        if (chest.get_inventory(defines.inventory.chest).is_full()) then
+          chest = spawn_new_chest()
+        end
+        if (count > stack_size) then
+          chest.insert({name=item_name, count=stack_size})
+          count = count - stack_size
+        else
+          chest.insert({name=item_name, count=count})
+          count = 0
+        end
+      end
+    else
+      global.hoorn_debug_string = global.hoorn_debug_string or {}
+      table.insert(global.hoorn_debug_string, "Item Prototype for " .. item_name .. " does not exist.")
+    end
+  end
+end
+
+script.on_event(defines.events.on_player_created, function (event)
+  if global.hoorn_debug_string then
+    for _, message in ipairs(global.hoorn_debug_string) do
+      game.print(message)
+    end
+    global.hoorn_debug_string = nil
+  end
+end)
+
+script.on_init(function (event)
+  if (not settings.startup["hoorn-enable-fast-start"]) then
+     return
+  end
+  logistic_items = {
+    {"transport-belt", 8000},
+    {"splitter", 150},
+    {"underground-belt", 700},
+    {"iron-chest", 120},
+    {"Construction Drone", 10}
+  }
+
+  production_items = {
+    {"electric-mining-drill", 400},
+    {"stone-furnace", 250},
+    {"inserter", 1000},
+    {"assembling-machine-1", 200},
+    {"ore-crusher", 50}
+  }
+
+  power_items = {
+      {"offshore-pump", 1},
+      {"boiler", 20},
+      {"steam-engine", 40},
+      {"pipe", 9},
+      {"pipe-to-ground", 2},
+      {"small-electric-pole", 200},
+      {"burner-turbine", 1}
+  }
+
+  other_items = {
+    {"car", 1}
+  }
+  
+  give_items_to_player_through_chests(logistic_items)
+  give_items_to_player_through_chests(production_items)
+  give_items_to_player_through_chests(power_items)
+  give_items_to_player_through_chests(other_items)
+
+  research_tech()
+end)
---
+++ settings.lua
@@ -0,0 +1,10 @@
+--ENABLED OR NOT
+data:extend({
+  {
+    type = "bool-setting",
+    name = "hoorn-enable-fast-start",
+    setting_type = "startup",
+    default_value = true,
+    order = "a"
+  },
+})
